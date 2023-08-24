be1fec03d3234b3f580971345ad84463207312d7
BrunoNT10 <brunont1211@gmail.com>
2023-08-24 18:39:03 -0300

    Implement: create initial configs

---
+++ main.py
@@ -0,0 +1,30 @@
+# %%
+import requests
+import pandas as pd
+import numpy as np
+
+
+# %%
+def parse_year(msg):
+    year = msg.split("/")[-1]
+    
+    return year
+
+csv_path = "dataset/brasileirao_info.csv"
+
+df = pd.read_csv(csv_path)
+
+anos = list(set(list(df.data.apply(parse_year))))
+
+times = list(set(list(df['mandante'])))
+
+
+# for i, row in df.iterrows():
+    
+# for i, row in df.iterrows():
+#     if not row.vencedor == "-":
+#         print(f"{row.mandante} x {row.visitante} - vencedor: {row.vencedor}")
+    
+#     else:
+#         print(f"{row.mandante} x {row.visitante} - empate")
+    
