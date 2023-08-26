dc557fbd2ea76ae77ea8db979f737eb9e5e9597b
BrunoNT10 <brunont1211@gmail.com>
2023-08-25 22:22:25 -0300

    Improve: create new df with teams pontuation

--- main.py
+++ main.py
@@ -14,19 +14,38 @@ csv_path = "dataset/brasileirao_info.csv"
 
 df = pd.read_csv(csv_path)
 
-anos = list(set(list(df.data.apply(parse_year))))
+anos = sorted(list(set(list(df.data.apply(parse_year)))))
 
-times = list(set(list(df['mandante'])))
+times = sorted(list(set(list(df['mandante']))))
 
-print(anos)
-print(times)
 
-# for i, row in df.iterrows():
+# %%
+
+df['pont_mandante'] = ["-"] * 8025
+df['pont_visitante'] = ["-"] * 8025
+
+print(df)
+# hhjsjsj
+
+for ano in anos:
+    team_points = {}
+    for time in times:
+        team_points[time] = 0
+        for i, row in df.iterrows():
+            if row.data.split("/")[-1] == ano:
+                if row.mandante == time or row.visitante == time:
+                    if row.mandante == time:
+                        df.at[i, 'pont_mandante'] = team_points[time]                                
+                    else:
+                        df.at[i, 'pont_visitante'] = team_points[time]
+                        
+                    if row.vencedor == time:
+                        team_points[time] += 3
+                    elif row.vencedor == '-':
+                        team_points[time] += 1
+            
+        print(team_points)
     
-# for i, row in df.iterrows():
-#     if not row.vencedor == "-":
-#         print(f"{row.mandante} x {row.visitante} - vencedor: {row.vencedor}")
-    
-#     else:
-#         print(f"{row.mandante} x {row.visitante} - empate")
-    
+
+
+df.to_csv("updated_pontuation.csv")
